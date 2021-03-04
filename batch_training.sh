3d1b44cc097122e1e913bad8a64b10aad3956320
Antonio Andriella <aandriella@iri.upc.edu>
2021-03-04 21:55:37 +0100

    runner for training CARESSER

---
+++ batch_training.sh
@@ -0,0 +1,34 @@
+#N.B.: before running it move the tpi folders in therapist-patient interaction
+#python main.py --epoch 5 --run 50 --user_id 1 --f True --s 0
+#python main.py --epoch 5 --run 50 --user_id 1 --f False --s 0
+#python main.py --epoch 5 --run 50 --user_id 2 --f True --s 0
+#python main.py --epoch 5 --run 50 --user_id 2 --f False --s 0
+#python main.py --epoch 5 --run 50 --user_id 3 --f True --s 0
+#python main.py --epoch 5 --run 50 --user_id 3 --f False --s 0
+#python main.py --epoch 5 --run 50 --user_id 5 --f True --s 0
+#python main.py --epoch 5 --run 50 --user_id 5 --f False --s 0
+#python main.py --epoch 5 --run 50 --user_id 6 --f True --s 0
+#python main.py --epoch 5 --run 50 --user_id 6 --f False --s 0
+#python main.py --epoch 5 --run 50 --user_id 7 --f True --s 0
+#python main.py --epoch 5 --run 50 --user_id 7 --f False --s 0
+#python main.py --epoch 5 --run 50 --user_id 8 --f True --s 0
+#python main.py --epoch 5 --run 50 --user_id 8 --f False --s 0
+#python main.py --epoch 5 --run 50 --user_id 9 --f True --s 0
+#python main.py --epoch 5 --run 50 --user_id 9 --f False --s 0
+#python main.py --epoch 5 --run 50 --user_id 10 --f True --s 0
+#python main.py --epoch 5 --run 50 --user_id 10 --f False --s 0
+
+python main.py --epoch 5 --run 50 --user_id 19 --f True --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 19 --f False --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 20 --f True --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 20 --f False --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 21 --f True --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 21 --f False --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 22 --f True --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 22 --f False --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 23 --f True --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 23 --f False --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 24 --f True --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 24 --f False --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 25 --f True --s 0 --objective neutral
+python main.py --epoch 5 --run 50 --user_id 25 --f False --s 0 --objective neutral
